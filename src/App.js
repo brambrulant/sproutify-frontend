2b6f043b2a9c60d396b0563c96d19bc7ef4bfefb
bramschabbink@hotmail.com <bramschabbink@hotmail.com>
2021-01-14 00:08:57 +0100

    deleted double lines

--- src/App.js
+++ src/App.js
@@ -1,28 +1,8 @@
 import { Route, Switch } from "react-router-dom";
 import Login from "./components/login";
 import Home from "./components/home";
-import queryString from "querystring";
-import axios from "axios";
 
 function App() {
-  const query = queryString.parse(window.location.search);
-  const token = query["?access_token"];
-  console.log(token);
-
-  getMe();
-
-  async function getMe() {
-    await axios
-      .get("https://api.spotify.com/v1/me/top/tracks", {
-        headers: {
-          Authorization: "Bearer" + token,
-        },
-      })
-      .then(function (response) {
-        console.log("top tracks?", response);
-      });
-  }
-
   return (
     <div className="App-header">
       <Switch>
