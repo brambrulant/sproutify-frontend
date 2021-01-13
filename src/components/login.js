d27c28ec63ce54ecf4ee8a32e96352ae5043b666
bramschabbink@hotmail.com <bramschabbink@hotmail.com>
2021-01-13 16:24:43 +0100

    changed href localhost to heroku

--- src/components/login.js
+++ src/components/login.js
@@ -4,14 +4,14 @@ const Login = (props) => {
   console.log("whatever in login component?", props.props);
   if (!props.props) {
     return (
-      <a className="login-a" href="http://localhost:4000">
+      <a className="login-a" href="https://sproutify-backend.herokuapp.com/">
         {" "}
         LOGIN
       </a>
     );
   } else {
     return (
-      <a className="login-a" href="http://localhost:4000">
+      <a className="login-a" href="https://sproutify-backend.herokuapp.com/">
         {" "}
         LOGOUT
       </a>
