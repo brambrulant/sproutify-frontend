5f2c6ce0faa383556fa29289f03aedbfb0515ef5
bramschabbink@hotmail.com <bramschabbink@hotmail.com>
2021-01-19 12:27:49 +0100

    added average bars/energy/instruments to sketch

--- src/sketches/sketch4.js
+++ src/sketches/sketch4.js
@@ -2,6 +2,8 @@ export default function sketch4(p) {
   const width = 1400; // declare width of canvas
   const height = 800; // declare height of canvas
   let angle;
+  let thickness;
+  let n;
 
   p.setup = function (props) {
     p.createCanvas(width, height);
@@ -13,16 +15,26 @@ export default function sketch4(p) {
 
   p.myCustomRedrawAccordingToNewPropsHandler = function (tracks) {
     if (tracks.tracks.length === 50) {
+      p.background(240, 232, 221);
       console.log(tracks.tracks);
-      let total = 0;
+      let totalBars = 0;
+      let totalEnergy = 0;
+      let totalInstrumentalness = 0;
       for (let i = 0; i < tracks.tracks.length; i++) {
-        total += tracks.tracks[i].analysis.bars.length;
+        totalBars += tracks.tracks[i].analysis.bars.length;
+        totalEnergy += tracks.tracks[i].features.energy;
+        totalInstrumentalness += tracks.tracks[i].features.instrumentalness;
       }
-      angle = total / tracks.tracks.length;
-      console.log("average", angle);
-      p.tree(width / 2, height, 300, angle, 0.5, 3, 0);
-      p.noLoop();
-    } else {
+      angle = totalBars / tracks.tracks.length;
+      thickness = p.map(totalEnergy / tracks.tracks.length, 0, 1, 1, 10);
+      n = p.map(totalInstrumentalness / tracks.tracks.length, 0, 1, 1, 5);
+      console.log("angle", angle);
+      console.log("thickness", thickness);
+      console.log("n", n);
+      p.tree(width / 2, height, 300, angle, 0.5, n, thickness, 0);
+      // p.noLoop();
+    } else if (tracks.tracks.length < 50) {
+      p.text("loading...", width / 2, height / 2, 200, 200);
     }
   };
 
@@ -31,15 +43,25 @@ export default function sketch4(p) {
     p.fill(255);
   };
 
-  p.tree = function (x, y, len, angle, scl, n, startingColor) {
+  p.tree = function (x, y, len, angle, scl, n, thickness, startingColor) {
     p.stroke(startingColor, 100, 100);
+    p.strokeWeight(thickness);
     p.line(0, 0, 0, -len);
     p.translate(0, -len);
     if (len > 1) {
       for (let i = 0; i < n; i++) {
         p.push();
         p.rotate(-angle + (2 * angle * i) / (n - 1));
-        p.tree(0, 0, len * scl, angle, scl, n, startingColor + 20);
+        p.tree(
+          0,
+          0,
+          len * scl,
+          angle,
+          scl,
+          n,
+          thickness - 2,
+          startingColor + 20
+        );
         p.pop();
       }
     }
