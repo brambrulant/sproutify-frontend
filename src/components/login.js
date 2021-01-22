b00b414721100c6d1746617775dd0f7b1a637820
bramschabbink@hotmail.com <bramschabbink@hotmail.com>
2021-01-22 04:02:45 +0100

    added gifLogo to loginPage

--- src/components/login.js
+++ src/components/login.js
@@ -1,16 +1,21 @@
 import React from "react";
+import logo from "../sproutify.gif";
 
 const Login = () => {
   console.log("backendlogin .env", process.env.REACT_APP_BACKEND_LOGIN);
   return (
-    <div className="Login-header">
+    <div className="ui">
+      <img src={logo} alt="logo" width="500" height="300" />
+      <div className="custom-h4">
+        fractal trees, based on your spotify behaviour
+      </div>
       <button
         className="big-button"
         onClick={() =>
           (window.location.href = process.env.REACT_APP_BACKEND_LOGIN)
         }
       >
-        Login
+        Login to Spotify
       </button>
     </div>
   );
